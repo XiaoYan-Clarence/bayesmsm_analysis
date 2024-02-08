6a5c7c0f3b3fc14693af216a253c47e0c00bfcfd
Kuan Liu <kuan.liu@mail.utoronto.ca>
2024-02-08 13:51:55 -0500

    Update bayesm_bootstrap.R

--- R/bayesm_bootstrap.R
+++ R/bayesm_bootstrap.R
@@ -13,22 +13,43 @@
 #' @return A list of summary measures (mean, sd, quantile)
 #' @export
 #'
-bayesm_bootstrap <- function(outcomevar = Y, # Do we really need this? outcomevar is variables$response extracted from data
-                             ymodel = "myoutcome ~ thing1 + a1^2 + thing3",
-                             intervention = list(ref_int = c(rep(0,n_visits)), comparator = c(rep(1,n_visits))), # just an example for 2 vectors of treatments
+bayesm_bootstrap <- function(ymodel = "myoutcome ~ thing1 + a1^2 + thing3",
                              nvisit = 10,
+                             intervention = list(ref_int = c(rep(0,n_visits)),
+                                                 comparator = c(rep(1,n_visits))), # just an example for 2 vectors of treatments
                              ref_int = c(rep(0,n_visits)), # An example of never treated
                              family = "gaussian", # "gaussian" or "binomial"
                              data = testdata,
                              wmean = rep(1, 1000),
                              nboot = 1000,
-                             optim_method = 'BFGS') {
+                             optim_method = 'BFGS'){
+
+  #testing;
+  ymodel = y ~ a_1+a_2+a_1*a_2*a_3 + a_2*a_3;
+  nvisit = 3;
+  intervention = list(ref_int = c(rep(0,nvisit)),
+                      comparator = c(rep(1,nvisit))); # just an example for 2 vectors of treatments
+  ref_int = c(rep(0,nvisit)); # An example of never treated
+  family = "gaussian";
+  data = testdata;
+  wmean = rep(1, 1000);
+  nboot = 1000;
+  optim_method = 'BFGS';
+
+  # first thing first load all the required R packages;
+  library(MCMCpack)
+
 
   # check
   # return error message if the input weight vector has different length comparing to the outcome Y;
   if (length(wmean) != nrow(data)) {
     stop("The length of the weight vector does not match the length of Y.")
   }
+
+  # # return error message if ref_int or intervention n_visit differenct from nvisit;
+  # if (xxx) {
+  #   stop("The potential outcome treatment sequence does not match with nvisit in length")
+  # }
 
   extract_variables <- function(formula) {
     # Get the terms of the formula
@@ -52,12 +73,24 @@ bayesm_bootstrap <- function(outcomevar = Y, # Do we really need this? outcomeva
 
   variables <- extract_variables(ymodel) # Extract variable names from the formula
   Y_name <- variables$response
-  A_name <- variables$predictors
 
   Y <- data[[Y_name]]
-  A <- cbind(1, data[, A_name]) # How to deal with interaction terms like thing1:thing2?
+  A_base <- data.frame(matrix(data = NA,
+                   nrow = nrow(data),
+                   ncol = length(variables$predictors)))
+  for (i in 1:length(variables$predictors)){
+    initial_vector <- variables$predictors[i]
+    split_vector <- strsplit(initial_vector, ":")
+    new_vector <- unlist(split_vector)
+    if (length(new_vector)==1){
+      A_base[,i] <-  data[, new_vector]
+    } else if (length(new_vector)>1){
+      A_base[,i] <-  apply(data[, new_vector],1,prod)
+    }
+  }
 
-
+  A <- cbind(1, A_base)
+  colnames(A)[2:ncol(A)]<- variables$predictors
 
   wloglik_normal<-function(param,
                            Y,
@@ -75,8 +108,6 @@ bayesm_bootstrap <- function(outcomevar = Y, # Do we really need this? outcomeva
     return(wlogl)
   }
 
-
-
   if (family == "gaussian"){
     wfn = wloglik_normal
   } else if (family == "binomial"){
@@ -88,9 +119,11 @@ bayesm_bootstrap <- function(outcomevar = Y, # Do we really need this? outcomeva
   bootest <- numeric(nboot)
 
   for (j in 1:nboot) {
+
+    j = 1;
     alpha <- as.numeric(rdirichlet(1, rep(1.0, length(Y))))
 
-    inits1 <- c(rep(0.1, length(A_name)), 4)  # Default initial values
+    inits1 <- c(rep(0.1, length(A)), 4)  # Default initial values
 
     maxim <- optim(inits1,
                    fn = wfn,
@@ -100,9 +133,17 @@ bayesm_bootstrap <- function(outcomevar = Y, # Do we really need this? outcomeva
                    control = list(fnscale = -1),
                    method = optim_method,
                    hessian = FALSE)
+    #Feb 8th, 2024;
+    maxim$par
+    [1]  2.40706022 -1.33106703 -2.31545410 -0.09949307  0.58522148  0.32738170  0.14387898 -0.22532452
+    [9] -1.14763173
+    y= intercept+ "a_1"+"a_2"+"a_3" + "a_1:a_2"+"a_1:a_3"+"a_2:a_3"+"a_1:a_2:a_3"
+    ref_int <- c(0,1,1)
+    y given a_1 = 0, a_2=1, a_3=0 what is that value given the parameter estimates =
 
-    effect_ref_int <- sum(maxim$par[intervention$ref_int]) # Example: ref_int = c(0,0), never treated for 2 visits
-    effect_comparator <- sum(maxim$par[intervention$comparator]) # Example: comparator = c(1,1), always treated for 2 visits
+      # adding empty initation similar to bootest outside the loop;
+    effect_ref_int[j] <- sum(maxim$par[intervention$ref_int]) # Example: ref_int = c(0,0), never treated for 2 visits
+    effect_comparator[j] <- sum(maxim$par[intervention$comparator]) # Example: comparator = c(1,1), always treated for 2 visits
     bootest[j] <- effect_comparator - effect_ref_int # Not sure if this is correct; for example always treated vs never treated
     # Sum the effects for all treatment variables
 
@@ -116,7 +157,23 @@ bayesm_bootstrap <- function(outcomevar = Y, # Do we really need this? outcomeva
     sd = sqrt(var(bootest)),
     quantile = quantile(bootest, probs = c(0.025, 0.975))
   ))
+
+  #also save output dataframe this will have 3 columns each corrsponding to effect_ref_int,effect_comparator, difference;
 }
 
+
+#test;
+testdata <- readr::read_csv("R/continuous_outcome_data.csv")
+testdata$a_3 <- rbinom(n=length(testdata$y),1,p=0.4)
+bayesm_bootstrap <- function(ymodel = "y ~ a_1+a_2+a_1*a_2*a_3 + a_2*a_3",
+                             nvisit = 10,
+                             intervention = list(ref_int = c(rep(0,n_visits)),
+                                                 comparator = c(rep(1,n_visits))), # just an example for 2 vectors of treatments
+                             ref_int = c(rep(0,n_visits)), # An example of never treated
+                             family = "gaussian", # "gaussian" or "binomial"
+                             data = testdata,
+                             wmean = rep(1, 1000),
+                             nboot = 1000,
+                             optim_method = 'BFGS')
 
 
